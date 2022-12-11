ed52a99747c4db79b3c88a149ca149727e47f405
Robert <robertandreedy@gmail.com>
2022-12-10 19:45:30 -0500

    additions to  user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,25 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = new Sequelize("../config/connection.js");
 
-const User = sequelize.define("User", {
-    id: {
-        primaryKey: true,
-        allowNull: false,
-        type: DataTypes.INTEGER
-    } 
-});
+const User = sequelize.define("User",
+	{
+		id: {
+			primaryKey: true,
+			allowNull: false,
+			type: DataTypes.INTEGER,
+			autoIncrement: true
+		},
+		username: {
+			allowNull: false,
+			type: DataTypes.STRING(20),
+			unique: true
+		},
+		password: {
+			allowNull: false,
+			type: DataTypes.STRING(30)
+		}
+	},
+	{
+
+	}
+);
